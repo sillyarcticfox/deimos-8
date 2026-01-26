39b0d622cc024917c80898a3cf2c8a388fd293b0
Sillyarcticfox <zachthearcticfox@proton.me>
2026-01-26 15:58:10 +0000

    6

--- src/assemble/main.py
+++ src/assemble/main.py
@@ -1,3 +1,21 @@
+"""
+Assembles code for the Deimos-8 CPU.
+Copyright (C) 2026  Sillyarcticfox
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import re, zlib, base64
 
 def u8(n: int) -> int: return n & 0xFF
